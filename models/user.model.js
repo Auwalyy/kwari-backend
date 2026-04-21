42f91bf9a158e4f8cfe3b59c6146d0e92edac9ea
Auwalu Muhammad Izziddin <auwalmuhammad5559@gmail.com>
2026-04-21 10:24:07 +0100

    users model

---
+++ models/user.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+
+    },
+    phone: {
+        type: String,
+        required: true, 
+    },
+    role: {
+        type: String,
+        enum: ['trader', 'employee', "customer", "admin"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+})
